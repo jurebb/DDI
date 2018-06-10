a095db3307282b371d98f620913f29f3bbbd5ca0
Jure Baban <baban.jure@gmail.com>
2018-06-10 22:29:09 +0200

    all task1 models

--- task1.py
+++ task1.py
@@ -14,10 +14,47 @@ from scipy.sparse import dia_matrix
 import gc
 from tempfile import mkdtemp
 import os.path as path
+from keras.layers import LSTM, Dense, Embedding, Input
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.optimizers import Adam
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import ensemble
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.externals import joblib
 
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
-__PARENT_PATH = '../Train/DrugBank/'
+from gensim.models import KeyedVectors
+
+__PARENT_PATH = 'semeval_task9_train\\Train\\MedLine\\'
+#__PARENT_PATH = '../Train/DrugBank/'
 __CHEMSPOT_PATH = '../molminer_vir/task1_1_chemspot_test.txt'
+
+
+def simple_nn(x_train, x_test, y_train, n_classes = 4):
+
+    print(x_train.shape)
+
+    model = Sequential()
+
+    model.add(Dense(100, input_dim = x_train.shape[1]))
+    model.add(Activation('relu'))
+
+    model.add(Dense(n_classes))
+    model.add(Activation('softmax'))
+
+    adam = Adam(lr = 0.01)
+
+    model.compile(loss = 'categorical_crossentropy', optimizer = adam, metrics=['accuracy']) 
+
+    model.fit(x_train, y_train, nb_epoch = 13, batch_size = 100)
+
+    pred = model.predict_classes(x_test, 100) #pred_temp je formata [[1], [0.5], [-0.7], ...] umjesto normalnog niza predikcija [1, 0.5, -0.7, ...] 
+
+    return pred
 
 
 def dfFromXML():
@@ -260,11 +297,7 @@ def task_1_tuttocompleto(df):
     x_train = np.concatenate((x_train, token_name_train), axis=1)
     x_test = np.concatenate((x_test, token_name_test), axis=1)
      
-    print('x_train.shape', x_train.shape)
-    
-    y_train = df_train['entity_name'].as_matrix()
-    y_test = df_test['entity_name'].as_matrix()
-    print('y_train.shape', y_train.shape)
+
     del sw
     del vectorizer
     del token_name_train
@@ -272,12 +305,68 @@ def task_1_tuttocompleto(df):
     del df2
     del data
     #return x_train, x_test, y_train, y_test, df_train['token_tag'], df_test['token_tag']
-    svc = LinearSVC(C =0.0004, class_weight = 'balanced')
+    y_train = df_train['entity_name'].astype("category").cat.codes.as_matrix()
+    y_test = df_test['entity_name'].astype("category").cat.codes.as_matrix()
+
+    lb = LabelBinarizer()
+
+    y_train = lb.fit_transform(y_train)
+    y_test = lb.transform(y_test)
+    
+    pred = simple_nn(x_train, x_test, y_train, 5)
+
+    #pred = lb.inverse_transform(pred)
+    y_train = lb.inverse_transform(y_train)
+    y_test = lb.inverse_transform(y_test)
+ 
+    pred_list = [pred]
+
+    print(accuracy_score(pred, y_test))
+    print(f1_score(pred, y_test, average = 'macro'))
+
+    lgr = LogisticRegression(C = 0.05, class_weight = 'balanced')
+    lgr.fit(x_train, y_train)
+    pred1 = lgr.predict(x_test)
+
+    pred_list.append(pred1)
+
+    print(accuracy_score(pred1, y_test))
+    print(f1_score(pred1, y_test, average = 'macro'))
+
+    svc = LinearSVC(C = 0.0004, class_weight = 'balanced')
     svc.fit(x_train, y_train)
-    pred = svc.predict(x_test)
+    pred2 = svc.predict(x_test)
+
+    pred_list.append(pred2)
+
+    print(accuracy_score(pred2, y_test))
+    print(f1_score(pred2, y_test, average = 'macro'))
+
+
+    #gb = ensemble.GradientBoostingClassifier()
+    #gb.fit(x_train, y_train)
+    #pred = gb.predict(x_test)
+
+    #pred_list.append(pred)
+
+    final_pred = []
+    for i in range(len(pred_list[0])):
+        temp = [0, 0, 0, 0, 0]
+        for j in range(len(pred_list)):
+            temp[pred_list[j][i]] += 1
+
+        final_pred.append(np.argmax(temp))
+    
+    pred = final_pred
+
     print(pred)
     print(accuracy_score(pred, y_test))
     print(f1_score(pred, y_test, average = 'macro'))
+
+    np.save('my_pred', pred)
+    np.save('y_test', y_test)
+
+    joblib.dump(lb, 'lb')
     
     
 def task1_1_nltk(df):    
@@ -307,6 +396,8 @@ def task1_1_nltk(df):
     
     text_train = df_train['sentence_text'].as_matrix()
     text_test = df_test['sentence_text'].as_matrix()
+
+    id_test = df_test['sentence_id'].as_matrix()
     
     print('text_train.shape', text_train.shape)
     
@@ -345,6 +436,10 @@ def task1_1_nltk(df):
     svc = LinearSVC(C =0.0004, class_weight = 'balanced')
     svc.fit(x_train, y_train)
     pred = svc.predict(x_test)
+
+    np.save('my_pred', pred)
+    np.save('y_test', y_test)
+    np.save('sentence_id', id_test)
     
     
     print(pred)
@@ -525,14 +620,68 @@ def task1_2(df):
     x_test = np.concatenate((x_test, dftest_tag), axis=1)
     #print(x_train[0:6])
     
-    y_train = df_train['entity_type'].as_matrix()
-    y_test = df_test['entity_type'].as_matrix()
+    y_train = df_train['entity_type'].astype("category").cat.codes.as_matrix()
+    y_test = df_test['entity_type'].astype("category").cat.codes.as_matrix()
+
+    lb = LabelBinarizer()
+
+    y_train = lb.fit_transform(y_train)
+    y_test = lb.transform(y_test)
     
-    svc = LinearSVC(C =0.0004, class_weight = 'balanced')
+    pred = simple_nn(x_train, x_test, y_train)
+
+    #pred = lb.inverse_transform(pred)
+    y_train = lb.inverse_transform(y_train)
+    y_test = lb.inverse_transform(y_test)
+ 
+    pred_list = [pred]
+
+    print(accuracy_score(pred, y_test))
+    print(f1_score(pred, y_test, average = 'macro'))
+
+    lgr = LogisticRegression(C = 0.05, class_weight = 'balanced')
+    lgr.fit(x_train, y_train)
+    pred1 = lgr.predict(x_test)
+
+    pred_list.append(pred1)
+
+    print(accuracy_score(pred1, y_test))
+    print(f1_score(pred1, y_test, average = 'macro'))
+
+    svc = LinearSVC(C = 0.0004, class_weight = 'balanced')
     svc.fit(x_train, y_train)
-    pred = svc.predict(x_test)
+    pred2 = svc.predict(x_test)
+
+    pred_list.append(pred2)
+
+    print(accuracy_score(pred2, y_test))
+    print(f1_score(pred2, y_test, average = 'macro'))
+
+    rfc = ensemble.RandomForestClassifier(class_weight = 'balanced')
+    rfc.fit(x_train, y_train)
+    pred3 = rfc.predict(x_test)
+
+    pred_list.append(pred3)
+
+    print(accuracy_score(pred3, y_test))
+    print(f1_score(pred3, y_test, average = 'macro'))
+
+    #gb = ensemble.GradientBoostingClassifier()
+    #gb.fit(x_train, y_train)
+    #pred = gb.predict(x_test)
+
+    #pred_list.append(pred)
+
+    final_pred = []
+    for i in range(len(pred_list[0])):
+        temp = [0, 0, 0, 0]
+        for j in range(len(pred_list)):
+            temp[pred_list[j][i]] += 1
+
+        final_pred.append(np.argmax(temp))
     
-    
+    pred = final_pred
+
     print(pred)
     print(accuracy_score(pred, y_test))
     print(f1_score(pred, y_test, average = 'macro'))
@@ -562,7 +711,192 @@ def task1_2(df):
                 br[j] += 1
     print(br)
          
+def task1_1_sequence_lab(df):    
+    print("======================== task 1_1 =============================")
     
+    del df['entity_charOffset']
+    del df['entity_id']
+    del df['entity_type']
+    del df['sentence_id']
+    
+    print(df.head())
+    
+    
+    df_train, df_test = train_test_split(df, test_size = 0.2, random_state=22, shuffle = False)
+    
+    text_train = df_train['sentence_text'].as_matrix()
+    text_test = df_test['sentence_text'].as_matrix()
+    
+
+    
+#this is an attempt to concatenate token tags to the dataset, memory leak problems
+    #token_name_train = np.column_stack((token_name_train, df_train['token_tag']))
+    #token_name_test = np.column_stack((token_name_test, df_test['token_tag']))
+    #[:,None]
+
+    GLOVE_DIR = 'C:\\Users\\viktor\\Projects\\Python\\projektHSP\\glove.twitter.27B'
+    MAX_SEQUENCE_LENGTH = 30
+    MAX_NB_WORDS = 200000
+    VALIDATION_SPLIT = 0.2
+
+     
+    
+    y_train = df_train['entity_name'].as_matrix()
+    y_test = df_test['entity_name'].as_matrix()
+
+    y_temp = []
+    br = 0
+    for i in range(len(text_train)):
+        temp = []
+        for word in text_train[i].split():
+            if MAX_SEQUENCE_LENGTH - len(temp) > 0:
+                if word in y_train[i]:
+                    br += 1
+                    temp.append([0, 1])
+                else:
+                    temp.append([1, 0])
+        if MAX_SEQUENCE_LENGTH - len(temp) > 0:
+            for j in range(MAX_SEQUENCE_LENGTH - len(temp)):
+                temp.append([1, 0])
+        
+        y_temp.append(temp)
+
+    y_train = y_temp
+
+    y_temp = []
+    y_test2 = []
+    br = 0
+    for i in range(len(text_test)):
+        temp = []
+        temp2 = []
+        for word in text_test[i].split():
+            if MAX_SEQUENCE_LENGTH - len(temp) > 0:
+                if word in y_test[i]:
+                    br += 1
+                    temp.append([0, 1])
+                    temp2.append(1)
+                    
+                else:
+                    temp.append([1, 0])
+                    temp2.append(0)
+        if MAX_SEQUENCE_LENGTH - len(temp) > 0:
+            for j in range(MAX_SEQUENCE_LENGTH - len(temp)):
+                temp.append([1, 0])
+                temp2.append(0)
+        
+        y_temp.append(temp)
+        y_test2.append(temp2)
+
+    y_test = y_temp
+
+
+    texts = np.reshape(text_train, (-1))
+    text_test = np.reshape(text_test, (-1))
+
+    tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+    tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+
+    word_index = tokenizer.word_index
+
+    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+
+    y_train = np.reshape(y_train, (-1, MAX_SEQUENCE_LENGTH, 2))
+    y_test = np.reshape(y_test, (-1, MAX_SEQUENCE_LENGTH, 2))
+
+    x_train = data
+
+    print(x_train.shape)
+
+
+    sequences_test = tokenizer.texts_to_sequences(text_test)
+    x_test = pad_sequences(sequences_test, maxlen=MAX_SEQUENCE_LENGTH)
+
+    embeddings_index = {}
+    f = open(os.path.join(GLOVE_DIR, 'glove.twitter.27B.100d.txt'), encoding="utf8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    EMBEDDING_DIM = 100
+
+    print('Found %s word vectors.' % len(embeddings_index))
+
+    embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+
+    print(embedding_matrix.shape)
+
+    sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+
+    model = Sequential()
+
+    embedding_layer = Embedding(len(word_index) + 1,
+                                EMBEDDING_DIM,
+                                weights=[embedding_matrix],
+                                input_length=MAX_SEQUENCE_LENGTH,
+                                trainable=False)
+
+    model.add(embedding_layer)
+
+    model.add(LSTM(units=100, dropout=0.2, recurrent_dropout=0.2, return_sequences = True))
+    model.add(Activation('relu'))
+
+    model.add(Dense(2))
+    model.add(Activation('softmax'))
+
+    adam = Adam(lr=0.01)
+
+    print(y_train.shape)
+
+    class_weight = dict()
+    class_weight[0] = 0.1
+    class_weight[1] = 10
+
+
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'], sample_weight_mode="temporal")
+
+    sample_weight = np.ones((y_train.shape[0], y_train.shape[1]))
+    
+
+    for i in range(y_train.shape[0]):
+        for j in range(y_train.shape[1]):
+            if y_train[i][j][0] == 0: #bitan slucaj
+                sample_weight[i,j] = sample_weight[i,j] * 4
+            else:
+                sample_weight[i,j] = sample_weight[i,j] * 0.5
+    
+
+    model.fit(x_train, y_train, nb_epoch=10, batch_size=100, sample_weight=sample_weight)
+
+    print(model.evaluate(x_test, y_test))
+
+    pred = model.predict_classes(x_test)
+    y_test2 = np.asarray(y_test2)
+
+    pp_br = 0
+    for p in pred:
+        for pp in p:
+            if pp != 0:
+                pp_br += 1
+
+    print(pp_br)
+
+    print(pred.shape)
+    print(y_test2.shape)
+
+    print(f1_score(pred, y_test2, average = 'macro'))
+
+    pred = np.reshape(pred, (pred.shape[0] * pred.shape[1], 1))
+    y_test2 = np.reshape(y_test2, (pred.shape[0] * pred.shape[1], 1))
+
+    print(f1_score(pred, y_test2, average = 'macro'))
     
 if __name__=="__main__":
     df = dfFromXML()
